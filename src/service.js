044f3ab075f5a3e01c3d71d70d6138ea7b9aaf3c
Jason Ehmke <jayehmke@gmail.com>
2019-03-23 16:32:24 -0400

    release v3.0.0

--- src/service.js
+++ src/service.js
@@ -6,60 +6,13 @@ const createService = function Service(serviceOptions) {
   const instance = {};
 
   instance.create = async (params) => {
-    let modelToCreate = new Model(params);
-    if (typeof Model.beforeCreate === 'function') {
-      try {
-        modelToCreate = await Model.beforeCreate(modelToCreate);
-      } catch (e) {
-        log(e);
-        return false;
-      }
-    }
-    return modelToCreate.save()
-      .then(data => {
-        if (typeof Model.afterCreate === 'function') {
-          Model.afterCreate(data)
-            .catch(e => {
-              log(e);
-            })
-        }
-        return data;
-      })
+    return Model.create(params)
   };
 
-  instance.findById = params => Model.get(params.id);
+  instance.findById = params => Model.findByPk(params.id);
 
   instance.findOne = async (params) => {
-    const { primary_search, secondary_search } = params;
-
-    if (!primary_search) {
-      throw new Error('primary_search is required');
-    }
-    // const otherParams = JSON.parse(JSON.stringify(params));
-    // delete otherParams.primary_search;
-
-    const [pk, pk_val] = primary_search.split(':');
-    const query = Model.queryOne(pk).eq(pk_val);
-
-    if (secondary_search) {
-      const [sk, sk_val] = primary_search.split(':');
-      if (sk && sk_val) {
-        query.where(sk).eq(sk_val)
-      }
-    }
-    // Object.keys(otherParams).forEach(param => {
-    //   const filter = {};
-    //   filter[param] = otherParams[param];
-    //   query.filter(filter)
-    // });
-
-    // Object.keys(params).forEach((key) => {
-    //   if (key.startsWith('_') !== true) {
-    //     query.where([key, params[key]]);
-    //   }
-    // });
-    return query.exec();
-
+    return Model.findOne({ where: params });
   };
 
   instance.findByIds = (ids) => {
@@ -68,65 +21,26 @@ const createService = function Service(serviceOptions) {
   };
 
   instance.findCount = (params) => {
-    const { primary_search, secondary_search } = params;
-    if (!primary_search) {
-      throw new Error('primary_search is required');
-    }
-    // const otherParams = JSON.parse(JSON.stringify(params));
-    // delete otherParams.primary_search;
-
-    const [pk, pk_val] = primary_search.split(':');
-    const query = Model.queryOne(pk).eq(pk_val);
-    if (secondary_search) {
-      const [sk, sk_val] = primary_search.split(':');
-      if (sk && sk_val) {
-        query.where(sk).eq(sk_val)
-      }
-    }
-    // Object.keys(otherParams).forEach(param => {
-    //   const filter = {};
-    //   filter[param] = otherParams[param];
-    //   query.filter(filter)
-    // });
-    return query.count().exec();
+    return Model.count({
+      where: params,
+    })
   };
 
   instance.findList = async (params, options = {}) => {
     const { _start = 0, _end = 10, primary_search, secondary_search } = params;
-    if (!primary_search) {
-      throw new Error('primary_search is required');
-    }
+
     const { includeCount = true } = options;
     const queryOptions = {
       limit: _end - _start,
       offset: _start || 0,
       // order: { property: params._sort || "id", descending: params.descending === "DESC" },
     };
-    // const otherParams = JSON.parse(JSON.stringify(params));
-    // delete otherParams.primary_search;
 
-    const [pk, pk_val] = primary_search.split(':');
-    const query = Model.query(pk).eq(pk_val);
-
-    if (secondary_search) {
-      const [sk, sk_val] = primary_search.split(':');
-      if (sk && sk_val) {
-        query.where(sk).eq(sk_val)
-      }
-    }
-    // Object.keys(otherParams).forEach(param => {
-    //   const filter = {};
-    //   filter[param] = otherParams[param];
-    //   query.filter(filter)
-    // });
-    query.limit(queryOptions.limit);
-
-    // Object.keys(params).forEach((key) => {
-    //   if (key.startsWith('_') !== true) {
-    //     query.where([key, params[key]]);
-    //   }
-    // });
-    const entities = await query.exec();
+    const entities = await Model.findAll({
+      where: params,
+      limit: queryOptions.limit,
+      offset: queryOptions.offset,
+    });
     const countEntities = includeCount ? await instance.findCount(params) : entities.length;
     return {
       entities,
@@ -136,25 +50,21 @@ const createService = function Service(serviceOptions) {
 
   instance.update = async (params) => {
     const { id } = params;
-    return Model.update(id, params);
+    delete params.id;
+    return Model.update(params, {
+      where: { id },
+    });
   };
 
-  instance.delete = async (id, originalItem) => {
+  instance.delete = async (id) => {
     const item = await instance.findById({ id });
     if (!item) {
       return false
     }
 
-    return Model.delete(id)
-      .then(() => {
-        if (typeof Model.afterDelete === 'function') {
-          Model.afterDelete(originalItem)
-            .catch(e => {
-              log(e);
-            })
-        }
-        return originalItem;
-      })
+    return Model.destroy({
+      where: { id },
+    })
   };
 
   return instance;
